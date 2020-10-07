7a770d685beba266cd4a7a7ddaa671fcb47302dc
Fernando <fernando.ribeiro@ramper.com.br>
2020-10-07 17:10:13 -0300

    make it better

--- app/controllers/taks/envioEmail.js
+++ app/controllers/taks/envioEmail.js
@@ -7,9 +7,6 @@ var tarefaEnvio = () => {
 
     setTimeout(() => {
 
-        console.log("aqui");
-
-
         var enviarEmailLixo = new schedule.RecurrenceRule();
 
         enviarEmailLixo.hour = 20
@@ -18,19 +15,16 @@ var tarefaEnvio = () => {
         enviarEmailLixo.dayOfWeek = dia.getDay()
 
         if(enviarEmailLixo.dayOfWeek == 0 || enviarEmailLixo.dayOfWeek == 2 || enviarEmailLixo.dayOfWeek == 4) {
-
             schedule.scheduleJob(enviarEmailLixo , function(){
 
-                console.log("Envio de email aviso lixo (início previsto às 20:00)... " + new Date().toISOString());
+            console.log("Envio de email aviso lixo (início previsto às 20:00)... " + new Date().toISOString());
     
-                email()
+            email()
     
             })
        }       
-    },1000)
-       
+    },1000)       
 }
-
 module.exports = tarefaEnvio
 
        
