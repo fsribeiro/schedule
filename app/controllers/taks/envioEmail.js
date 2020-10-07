2f9357e0e5da7427f2149406e63427a00cc4756d
Fernando <fernando.ribeiro@ramper.com.br>
2020-10-07 18:51:30 -0300

    teste deploy

--- app/controllers/taks/envioEmail.js
+++ app/controllers/taks/envioEmail.js
@@ -9,12 +9,12 @@ var tarefaEnvio = () => {
 
         var enviarEmailLixo = new schedule.RecurrenceRule();
 
-        enviarEmailLixo.hour = 20
-        enviarEmailLixo.minute = 00
+        enviarEmailLixo.hour = 18
+        enviarEmailLixo.minute = 55
         let dia = new Date()
         enviarEmailLixo.dayOfWeek = dia.getDay()
 
-        if(enviarEmailLixo.dayOfWeek == 0 || enviarEmailLixo.dayOfWeek == 2 || enviarEmailLixo.dayOfWeek == 4) {
+        if(enviarEmailLixo.dayOfWeek == 0 || enviarEmailLixo.dayOfWeek == 2 || enviarEmailLixo.dayOfWeek == 3) {
             schedule.scheduleJob(enviarEmailLixo , function(){
 
             console.log("Envio de email aviso lixo (início previsto às 20:00)... " + new Date().toISOString());
