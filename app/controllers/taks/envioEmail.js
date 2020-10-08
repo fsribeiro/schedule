0de63b3aa88652ff2827b154b5b5b4cdd1918989
Fernando <fernando.ribeiro@ramper.com.br>
2020-10-07 22:25:42 -0300

    teste horário envio USA

--- app/controllers/taks/envioEmail.js
+++ app/controllers/taks/envioEmail.js
@@ -9,8 +9,8 @@ var tarefaEnvio = () => {
 
         var enviarEmailLixo = new schedule.RecurrenceRule();
 
-        enviarEmailLixo.hour = 19
-        enviarEmailLixo.minute = 15
+        enviarEmailLixo.hour = 21
+        enviarEmailLixo.minute = 30
         let dia = new Date()
         enviarEmailLixo.dayOfWeek = dia.getDay()
 
