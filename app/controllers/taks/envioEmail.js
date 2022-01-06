222bed1aeec53ef8b1ab71079f7688fa6a953fa7
Fernando <fernando.ribeiro@ramper.com.br>
2022-01-06 17:24:39 -0300

    teste7

--- app/controllers/taks/envioEmail.js
+++ app/controllers/taks/envioEmail.js
@@ -7,7 +7,7 @@ const tarefaEnvio = () => {
     setTimeout(() => {
         let enviarEmailLixo = new schedule.RecurrenceRule();
         enviarEmailLixo.hour = 15;
-        enviarEmailLixo.minute = 25;
+        enviarEmailLixo.minute = 28;
 
         schedule.scheduleJob(enviarEmailLixo , function(){
             console.log("Envio de email aviso lixo (início previsto às 6:39)... " + new Date().toISOString());
